fbb38d6f4a3f4b0d61037e589e4bf2a81135718c
Loren Eteval <loren.eteval@proton.me>
2024-05-14 21:00:59 +0800

    Update job name

---
+++ main-without-qtnetwork.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import *
+
+
+app = QApplication()
+
+window = QMainWindow()
+window.show()
+
+app.exec()
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from PySide6 import QtCore
 from PySide6.QtWidgets import *
+from PySide6.QtNetwork import *
+
+import functools
+
+
+def handleFinishedByNetworkReply(networkReply):
+    print('finished')
 
 
 app = QApplication()
+
+manager = QNetworkAccessManager()
+reply = manager.get(QNetworkRequest(QtCore.QUrl('http://cp.cloudflare.com/')))
+reply.finished.connect(
+    functools.partial(
+        handleFinishedByNetworkReply,
+        reply,
+    )
+)
 
 window = QMainWindow()
 window.show()
